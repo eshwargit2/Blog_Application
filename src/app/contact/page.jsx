4dac1e8543c6d8b231571d2b32cbfd2ef078a417
eshwargit2 <msselectronic57@gmail.com>
2025-06-23 08:01:57 +0530

    update

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,20 @@
+"use client"
+
 import React from 'react'
+import { useState } from 'react'
 
 const page = () => {
+
+
+  const [inputs, setInputs]=useState("");
+
+
+  const handleInput=()=>{
+
+  }
+
+
+
   return (
     <div>
       <main className="container mx-auto px-4 py-6">
@@ -8,15 +22,15 @@ const page = () => {
         <form className="w-full max-w-lg">
             <div className="flex items-center mb-4">
                 <label htmlFor="name" className="w-1/4">Name:</label>
-                <input type="text" id="name" className="border rounded px-2 py-1 w-3/4"/>
+                <input type="text" id="name" name='name' onChange={handleInput} value={inputs.name??""} className="border rounded px-2 py-1 w-3/4"/>
             </div>
             <div className="flex items-center mb-4">
                 <label htmlFor="email" className="w-1/4">Email:</label>
-                <input type="email" id="email" className="border rounded px-2 py-1 w-3/4"/>
+                <input type="email" id="email" name='email'  onChange={handleInput} value={inputs.email??""}  className="border rounded px-2 py-1 w-3/4"/>
             </div>
             <div className="flex items-center mb-4">
                 <label htmlFor="message" className="w-1/4">Message:</label>
-                <textarea id="message" className="border rounded px-2 py-1 w-3/4" rows="4"></textarea>
+                <textarea id="message" name='message'  onChange={handleInput} value={inputs.message??""}  className="border rounded px-2 py-1 w-3/4" rows="4"></textarea>
             </div>
             <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Submit</button>
         </form>
